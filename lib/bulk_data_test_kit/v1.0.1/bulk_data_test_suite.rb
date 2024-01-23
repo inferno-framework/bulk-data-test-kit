e01604318fd24f88f878f51b64d18d8f89031e06
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2024-01-23 16:31:16 -0500

    Make SMART backend services an optional group, and then add all other bulk data export tests into their own required group so that they can be run independently of the SMART backend services group

--- lib/bulk_data_test_kit/v1.0.1/bulk_data_test_suite.rb
+++ lib/bulk_data_test_kit/v1.0.1/bulk_data_test_suite.rb
@@ -123,6 +123,7 @@ module BulkDataTestKit
       group do
         title 'SMART Backend Services'
         id :bulk_data_smart_backend_services_v101
+        optional
         run_as_group
   
         group from: :smart_discovery_stu2,
@@ -134,31 +135,36 @@ module BulkDataTestKit
       end
 
       group do
-        id :bulk_data_server_tests
-        title 'Bulk Data Server TLS Tests'
-        run_as_group
-        
-        test from: :tls_version_test do
-          title 'Bulk Data Server is secured by transport layer security'
-          description <<~DESCRIPTION
-            [§170.315(g)(10) Test
-            Procedure](https://www.healthit.gov/test-method/standardized-api-patient-and-population-services)
-            requires that all exchanges described herein between a client and a
-            server SHALL be secured using Transport Layer Security (TLS) Protocol
-            Version 1.2 (RFC5246).
-          DESCRIPTION
-          id :bulk_data_server_tls_version
+        id :bulk_data_export_tests
+        title 'Bulk Data Export Tests'
 
-          config(
-            inputs: { url: { name: :bulk_server_url } },
-            options: { minimum_allowed_version: OpenSSL::SSL::TLS1_2_VERSION }
-          )
+        group do
+          id :bulk_data_server_tests
+          title 'Bulk Data Server TLS Tests'
+          run_as_group
+          
+          test from: :tls_version_test do
+            title 'Bulk Data Server is secured by transport layer security'
+            description <<~DESCRIPTION
+              [§170.315(g)(10) Test
+              Procedure](https://www.healthit.gov/test-method/standardized-api-patient-and-population-services)
+              requires that all exchanges described herein between a client and a
+              server SHALL be secured using Transport Layer Security (TLS) Protocol
+              Version 1.2 (RFC5246).
+            DESCRIPTION
+            id :bulk_data_server_tls_version
+
+            config(
+              inputs: { url: { name: :bulk_server_url } },
+              options: { minimum_allowed_version: OpenSSL::SSL::TLS1_2_VERSION }
+            )
+          end
         end
-      end
 
-      group from: :bulk_data_group_export_v101
-      group from: :bulk_data_patient_export_v101
-      group from: :bulk_data_system_export_v101
+        group from: :bulk_data_group_export_v101
+        group from: :bulk_data_patient_export_v101
+        group from: :bulk_data_system_export_v101
+      end
     end
   end
 end
--- lib/bulk_data_test_kit/v2.0.0/bulk_data_test_suite.rb
+++ lib/bulk_data_test_kit/v2.0.0/bulk_data_test_suite.rb
@@ -124,6 +124,7 @@ module BulkDataTestKit
         title 'SMART Backend Services'
         id :bulk_data_smart_backend_services_v200
         run_as_group
+        optional
   
         group from: :smart_discovery_stu2,
         config: {
@@ -134,31 +135,36 @@ module BulkDataTestKit
       end
 
       group do
-        id :bulk_data_server_tests_stu2
-        title 'Bulk Data Server TLS Tests'
-        run_as_group
+        id :bulk_data_export_tests_stu2
+        title 'Bulk Data Export Tests'
         
-        test from: :tls_version_test do
-          title 'Bulk Data Server is secured by transport layer security'
-          description <<~DESCRIPTION
-            [§170.315(g)(10) Test
-            Procedure](https://www.healthit.gov/test-method/standardized-api-patient-and-population-services)
-            requires that all exchanges described herein between a client and a
-            server SHALL be secured using Transport Layer Security (TLS) Protocol
-            Version 1.2 (RFC5246).
-          DESCRIPTION
-          id :bulk_data_server_tls_version_stu2
+        group do
+          id :bulk_data_server_tests_stu2
+          title 'Bulk Data Server TLS Tests'
+          run_as_group
+          
+          test from: :tls_version_test do
+            title 'Bulk Data Server is secured by transport layer security'
+            description <<~DESCRIPTION
+              [§170.315(g)(10) Test
+              Procedure](https://www.healthit.gov/test-method/standardized-api-patient-and-population-services)
+              requires that all exchanges described herein between a client and a
+              server SHALL be secured using Transport Layer Security (TLS) Protocol
+              Version 1.2 (RFC5246).
+            DESCRIPTION
+            id :bulk_data_server_tls_version_stu2
 
-          config(
-            inputs: { url: { name: :bulk_server_url } },
-            options: { minimum_allowed_version: OpenSSL::SSL::TLS1_2_VERSION }
-          )
+            config(
+              inputs: { url: { name: :bulk_server_url } },
+              options: { minimum_allowed_version: OpenSSL::SSL::TLS1_2_VERSION }
+            )
+          end
         end
-      end
 
-      group from: :bulk_data_group_export_v200
-      group from: :bulk_data_patient_export_v200
-      group from: :bulk_data_system_export_v200
+        group from: :bulk_data_group_export_v200
+        group from: :bulk_data_patient_export_v200
+        group from: :bulk_data_system_export_v200
+      end
     end
   end
 end

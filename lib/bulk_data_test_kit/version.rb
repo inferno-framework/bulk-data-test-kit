b6df7a818ea6846799a3ed4c455be860dea4f83b
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-02-25 11:39:56 -0500

    Bump version to 0.11.0 (#40)

--- lib/bulk_data_test_kit/version.rb
+++ lib/bulk_data_test_kit/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module BulkDataTestKit
-  VERSION = '0.10.1'         # TODO breaking change
-  LAST_UPDATED = '2024-06-6'
+  VERSION = '0.11.0'        
+  LAST_UPDATED = '2025-02-24'
 end
